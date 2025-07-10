5d0949d50f936ee97f89abfba47631f2ac35dc9f
GeraGR <gerardo_1322134023@uptecamac.edu.mx>
2025-07-10 16:17:09 -0600

    Refactor DTOs and update AuthModel formatting
    Renamed 'Branch - Copia - Copia (2).cs' to 'AuthRequestDTO.cs' and moved it to the DTOs folder. Added a new 'LoginRequestDTO.cs' file with a namespace declaration. Reformatted property spacing in AuthModel for consistency.

--- Models/AuthModel.cs
+++ Models/AuthModel.cs
@@ -4,8 +4,9 @@
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public string Name { get; set; }  
-        public string LastName { get; set; }  
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
         public Guid CardBrandId { get; set; }
         public decimal Wallet { get; set; }
     }
